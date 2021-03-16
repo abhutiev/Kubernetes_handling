2b87acda79d356d9287e0b2452326697f24da418
Alex Hutiev <abhutiev@gmail.com>
2021-03-16 10:01:57 +0300

    script applies metallb config, changed structure

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 minikube start --vm-driver=docker
 minikube status
 minikube addons enable dashboard
+minikube addons enable metallb
+kubectl apply -f srcs/metallb.yaml
 minikube addons list
+minikube dashboard
