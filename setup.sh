d28a26f0c10641be255737c04a6f1727a5f06552
Alexander Hutiev <abhutiev@gmail.com>
2021-05-04 03:12:26 +0300

    v2.0

--- setup.sh
+++ setup.sh
@@ -13,56 +13,46 @@ eval $(minikube docker-env)
 #                   NGINX
 
 echo "Docker build Nginx"
-echo -e "While waiting for nginx start, you can think under question from Что? Где? Когда?"
-echo -n "⌛ В рассказе о сербской знахарке ЙовАнке корреспондент передачи \"Их нравы\" говорит, что она многое умеет:\
-лечит и сердце, и желудок, и почки, ну, а ДЕЛАТЬ ЭТО она умеет лучше всего. Какие два слова, начинающиеся на\
-одну и ту же букву, мы заменили словами \"ДЕЛАТЬ ЭТО\"?"
-docker build -t nginx_image srcs/nginx > /dev/null 2>&1
+docker build -t nginx_image srcs/nginx
 echo "Nginx started"
-echo "Ответ: Заговаривать зубы"
 
 #                   WORDPRESS
 
 echo "Docker build Wordpress"
-echo "While waiting for wordpress start, you can think under question from Что? Где? Когда?"
-echo "⌛ Картасуру, древнюю столицу яванского королевства, позднее сменила на этом посту соседняя Суракарта.\
-Назовите современное государство, события 1868 года в котором, по мнению сайта Wordsmith.Org,\
-стоят в одном ряду с упомянутым фактом."
-docker build -t wordpress_image srcs/wordpress > /dev/null 2>&1
-echo "Ответ: Япония"
+docker build -t wordpress_image srcs/wordpress
 echo "WordPress started"
 
 
 #                   MYSQL
 
 echo "Docker build Mysql"
-docker build -t mysql_image srcs/mysql > /dev/null 2>&1
+docker build -t mysql_image srcs/mysql
+echo "MySQL started"
 
 
 #                   PHPMYADMIN
 
 echo "Docker build Phpmyadmin"
-echo "While waiting for phpmyadmin restart, you can think under question from Что? Где? Когда?"
-echo "⌛ ЕЕ покупали заранее, мелкими партиями, причем — что выглядит особо цинично — в Западной Германии. \
-Вся ОНА пошла в дело за одну августовскую ночь. Назовите ЕЕ двумя словами.?"
 docker build -t phpmyadmin_image srcs/phpmyadmin > tmp 2>&1
-echo "Ответ: Колючая проволока"
 echo "PhpMyAdmin started"
 
 #                   FTPS
 
 echo "Docker build Ftps"
-docker build -t ftps_image srcs/ftps > /dev/null 2>&1
+docker build -t ftps_image srcs/ftps
+echo "FTPS started"
 
 #                   GRAPHANA
 
 echo "Docker build Grafana"
-docker build -t grafana_image srcs/grafana > /dev/null 2>&1
+docker build -t grafana_image srcs/grafana
+echo "Grafana started"
 
 #                   INFLUXDB
 
-echo "Docker build Influxdb\n\n"
-docker build -t influxdb_image srcs/influxdb > /dev/null 2>&1
+echo "Docker build Influxdb"
+docker build -t influxdb_image srcs/influxdb
+echo "InfluxDB started"
 
 
 #                   YAMLS
@@ -75,8 +65,10 @@ kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress/wordpress.yaml
 kubectl apply -f srcs/grafana/grafana.yaml
 kubectl apply -f srcs/influxdb/influxdb.yaml
+
 #  Create secret
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 #  Dashboard
 minikube dashboard
 
